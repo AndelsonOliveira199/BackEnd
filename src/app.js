10245dc01c28c30f7ccfccfcd40d1d64f33d80b3
AndelsonOliveira1995 <JOliveiraC1995@gmail.com>
2024-10-03 14:05:25 -0300

    ATUALIZAÇÃO

--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ const app = express();
 // CRIANDO AS ROTAS
 app.get("/", (request, response) => response.status(200).send("Olá Mundon"))
 
-app.listen(2024, () => console.log("Servidor rodando na porta 2024"));
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,3 @@
+const app = require("./app");
+
+app.listen(2024, () => console.log("Servidor rodando na porta 2024"));
